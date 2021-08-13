5f3a28c694407ab87f485aa25ee18d6fc367971c
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-08-12 22:58:27 -0700

    added components and styled body slightly

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route, Link } from "react-router-dom"
+const App = () => {
+    return (
+        <Switch>
+            <Route>
+
+            </Route>
+        </Switch>
+        
+    )
+}
+
+export default App
---
+++ src/components/H&L/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HoursLocations = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default index
